3ca0ae0ef1db00b7cc0a32ff6107baf74d691344
Thiago Ramos <trs.tiba@gmail.com>
2022-12-27 20:24:26 -0300

    Componente de fila de pedidos

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { Header } from './components/Header';
 import { GlobalStyles } from './styles/GlobalStyles';
+
+import { Header } from './components/Header';
+import { Order } from './components/Orders';
 
 export function App(){
   return  (
     <>
       <GlobalStyles />
       <Header />
+      <Order />
     </>
 
   );
