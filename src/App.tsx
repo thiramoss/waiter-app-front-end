5db1c085f6e9a88297dee9e3ea2c1f7f946a35ba
Thiago Ramos <trs.tiba@gmail.com>
2022-12-27 18:08:35 -0300

    Componente Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { Header } from './components/Header';
+import { GlobalStyles } from './styles/GlobalStyles';
+
 export function App(){
-  return  <h1>Hello World</h1>;
+  return  (
+    <>
+      <GlobalStyles />
+      <Header />
+    </>
+
+  );
 }
